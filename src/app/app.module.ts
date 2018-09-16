b958923fb506bf4fb1516a5d739571f6c5dacd83
sneha <snehapaulson.1993@gmail.com>
2018-09-16 17:27:35 +0530

    single page to multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurservicesComponent } from './ourservices/ourservices.component';
+import { OurTeamComponent } from './our-team/our-team.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes: Routes = [
+  { path: 'about-us', component:AboutUsComponent  },
+  {  path: 'our-services', component:OurservicesComponent  },
+  {  path: 'our-team', component:OurTeamComponent  },
+  {  path: 'contact-us', component:ContactUsComponent  },
+  {  path: 'home', component:HomeComponent  },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+  { path: '**',   redirectTo: '/home' }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    OurservicesComponent,
+    OurTeamComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+     
+    ),
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
